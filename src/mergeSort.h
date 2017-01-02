fa7e6a3b25e431a3e1e1e9aeb829ea0797077c4e
Don Nguyen <don78colorado@gmail.com>
2017-01-02 11:58:54 -0700

    split test into STLStyle and normal

--- src/mergeSort.h
+++ src/mergeSort.h
@@ -12,43 +12,39 @@
  */
 void merge(int intArray[], const std::size_t &end1, const std::size_t &end2);
 void mergeSort(int intArray[], const size_t &length);
-//void mergeSort(std::vector<int> &v);
-//void mergeSort(typename std::vector<int>::iterator start, typename std::vector<int>::iterator finish);
 
 void merge(std::vector<int>::iterator start, std::vector<int>::iterator end1,
            std::vector<int>::iterator end2);
 
-/*
-template <typename T>
-void merge(T &v, typename T::const_iterator end1)
+template <typename Container>
+void merge(Container &v, typename Container::const_iterator end1)
 {
-    typename T::const_iterator end2 = v.end();
-    typename T::iterator x;
-    typename T::const_iterator i, j;
-    T tempVector;
-    for (x = tempVector.begin(), i = v.begin(), j = end1+1; i <= end1 && j != end2 ; ) {
+    typename Container::const_iterator end2 = v.end();
+    typename Container::iterator x;
+    typename Container::const_iterator i, j;
+    Container tempContainer;
+    for (x = tempContainer.begin(), i = v.begin(), j = end1+1; i <= end1 && j != end2 ; ) {
         //std::cout << "*i:" << *i << " *j:" << *j << " end1:" << *end1 << " end2:" << *end2 << std::endl;
         if ((j==end2 || i <= end1) && (*i < *j)) {
-            tempVector.push_back(*i);
+            tempContainer.push_back(*i);
             i++;
         }
         else {
-            tempVector.push_back(*j);
+            tempContainer.push_back(*j);
             j++;
         }
     }
     // copy remaining
     while (i <= end1) {
-        tempVector.push_back(*i);
+        tempContainer.push_back(*i);
         i++;
     }
     while (j != end2) {
-        tempVector.push_back(*j);
+        tempContainer.push_back(*j);
         j++;
     }
-    std::copy(tempVector.begin(), tempVector.end(), v.begin());
+    std::copy(tempContainer.begin(), tempContainer.end(), v.begin());
 }
-*/
 
 /*
 template <class InputIterator>
@@ -71,10 +67,17 @@ void mergeSort(InputIterator begin, InputIterator end)
     merge(begin, i, end);
 }
 
-template <typename T>
-void mergeSort(std::vector<T> &v)
+template <typename Container>
+void mergeSort(Container &c)
 {
-    mergeSort(v.begin(),v.end());
+    typename Container::iterator i = c.begin();
+    while (i != c.end() && *i < *(i+1)) {
+        ++i;
+    }
+    if (i == c.end())
+        return;
+    mergeSort(i+1, c.end());
+    merge(c, i);
 }
 
 #endif // MERGESORT_H
--- src/test/mergesorttest.cpp
+++ src/test/mergesorttest.cpp
@@ -8,18 +8,20 @@ class TestMergeSort: public QObject
 private slots:
     void initTestCase()
     { qDebug("called before everything else"); }
-    void arrayTest();
-    void mergeTest();
+    void arrayMergeSortTest();
+    void arrayMergeTest();
     void stdArrayTest();
     void vectorTest();
+    void vectorSTLStyleTest();
     void mergeVectorTest();
+    void mergeVectorSTLStyleTest();
     void cleanupTestCase()
     { qDebug("called after all tests"); }
 };
 
 
 
-void TestMergeSort::arrayTest()
+void TestMergeSort::arrayMergeSortTest()
 {
     int testarray[] = {5, 10, 60, 2, 1243, 2343, 12, 2354, 6654, 23, 1};
     int sortedArray[] = {1, 2, 5, 10, 12, 23, 60, 1243, 2343, 2354, 6654};
@@ -28,7 +30,7 @@ void TestMergeSort::arrayTest()
     QVERIFY(std::equal(testarray, testarray+testArrayLength, sortedArray));
 }
 
-void TestMergeSort::mergeTest()
+void TestMergeSort::arrayMergeTest()
 {
     int testarray[] = {0,3,11,50,3,6,7,49,81};
     int sortedArray[] = {0,3,3,6,7,11,49,50,81};
@@ -65,7 +67,7 @@ void TestMergeSort::stdArrayTest()
 {
     std::array<int, 11> testarray {5, 10, 60, 2, 1243, 2343, 12, 2354, 6654, 23, 1};
     std::array<int, 11> sortedarray {1, 2, 5, 10, 12, 23, 60, 1243, 2343, 2354, 6654};
-    //mergeSort(testarray.begin(), testarray.end());
+    //mergeSort(testarray);
     QCOMPARE(testarray, sortedarray);
 }
 
@@ -76,7 +78,10 @@ void TestMergeSort::vectorTest()
     std::vector<int> sortedVector{1, 2, 5, 10, 12, 23, 60, 1243, 2343, 2354, 6654};
     mergeSort(intVector);
     QCOMPARE(intVector,sortedVector);
+}
 
+void TestMergeSort::vectorSTLStyleTest()
+{
     const std::size_t randomVectorLength = 10;
     const int maxRandomNumber = 100;
     std::vector<int> randomVector;
@@ -90,6 +95,58 @@ void TestMergeSort::vectorTest()
 }
 
 void TestMergeSort::mergeVectorTest()
+{
+    int testarray[] = {0,3,11,50,3,6,7,49,81};
+    std::vector<int> intVector(testarray, testarray+sizeof(testarray)/sizeof(testarray[0]));
+    std::vector<int>::iterator end1 = intVector.begin();
+    end1 = end1 + 3;
+    merge(intVector, end1);
+    QVERIFY(std::is_sorted(intVector.begin(), intVector.end()));
+
+    for (int i = 0; i < 50; ++i) {  // repeat 5 times
+        // creat two sorted std::vectors of length 10, combine them into an array and test merge on it
+        const std::size_t randomVectorLength = 10;
+        const int maxRandomNumber = 10;
+        std::vector<int> randomVector1;
+        std::generate_n(std::back_insert_iterator<std::vector<int>>(randomVector1),
+                        randomVectorLength, []() { return rand()%(maxRandomNumber+1); });
+        std::vector<int> randomVector2;
+        std::generate_n(std::back_insert_iterator<std::vector<int>>(randomVector2),
+                        randomVectorLength, []() { return rand()%(maxRandomNumber+1); });
+        std::sort(randomVector1.begin(), randomVector1.end());
+        std::vector<int>::iterator end1 = randomVector1.end();
+        std::sort(randomVector2.begin(), randomVector2.end());
+        randomVector1.insert(randomVector1.end(), randomVector2.begin(), randomVector2.end());
+        //print(randomVector1.begin(), randomVector1.end());
+        QVERIFY(!std::is_sorted(randomVector1.begin(), randomVector1.end()));
+        merge(randomVector1, randomVector1.begin() + randomVectorLength-1);
+        QVERIFY(std::is_sorted(randomVector1.begin(), randomVector1.end()));
+    }
+
+    const std::size_t vMin = 100;
+    const std::size_t vMax = 200;
+    const int maxRandomNumber = 25000;
+    for (int i = 0; i < 50; ++i) {
+        const std::size_t v1Length = vMin+rand()%(vMax-vMin+1);
+        const std::size_t v2Length = vMin+rand()%(vMax-vMin+1);
+        const std::size_t aLength = v1Length + v2Length;
+        std::vector<int> randomVector1;
+        std::generate_n(std::back_insert_iterator<std::vector<int>>(randomVector1),
+                        v1Length, []() { return rand()%(maxRandomNumber+1); });
+        std::vector<int> randomVector2;
+        std::generate_n(std::back_insert_iterator<std::vector<int>>(randomVector2),
+                        v2Length, []() { return rand()%(maxRandomNumber+1); });
+        std::sort(randomVector1.begin(), randomVector1.end());
+        std::sort(randomVector2.begin(), randomVector2.end());
+        randomVector1.insert(randomVector1.end(), randomVector2.begin(), randomVector2.end());
+        //print(randomVector1.begin(), randomVector1.end());
+        QVERIFY(!std::is_sorted(randomVector1.begin(), randomVector1.end()));
+        merge(randomVector1, randomVector1.begin() + v1Length-1);
+        QVERIFY(std::is_sorted(randomVector1.begin(), randomVector1.end()));
+    }
+}
+
+void TestMergeSort::mergeVectorSTLStyleTest()
 {
     int testarray[] = {0,3,11,50,3,6,7,49,81};
     std::vector<int> intVector(testarray, testarray+sizeof(testarray)/sizeof(testarray[0]));
@@ -136,7 +193,6 @@ void TestMergeSort::mergeVectorTest()
         randomVector1.insert(randomVector1.end(), randomVector2.begin(), randomVector2.end());
         //print(randomVector1.begin(), randomVector1.end());
         QVERIFY(!std::is_sorted(randomVector1.begin(), randomVector1.end()));
-        //merge(randomVector1, randomVector1.begin() + v1Length-1);
         merge(randomVector1.begin(), randomVector1.begin() + v1Length-1, randomVector1.end());
         QVERIFY(std::is_sorted(randomVector1.begin(), randomVector1.end()));
     }

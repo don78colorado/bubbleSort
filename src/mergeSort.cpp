65ce302745a9cd19c552bdab296298725ceedc15
Don Nguyen <don78colorado@gmail.com>
2017-01-02 18:46:56 -0700

    remove commented code

--- src/mergeSort.cpp
+++ src/mergeSort.cpp
@@ -1,35 +1,4 @@
 #include "mergeSort.h"
-#include <algorithm>
-//#include <iostream>
-
-void merge(std::vector<int>::iterator start, std::vector<int>::iterator end1,
-           std::vector<int>::iterator end2)
-{
-    std::vector<int>::iterator x;
-    std::vector<int>::const_iterator i, j;
-    std::vector<int> tempVector;
-    for (x = tempVector.begin(), i = start, j = end1+1; i <= end1 && j != end2 ; ) {
-        //std::cout << "*i:" << *i << " *j:" << *j << " end1:" << *end1 << " end2:" << *end2 << std::endl;
-        if ((j==end2 || i <= end1) && (*i < *j)) {
-            tempVector.push_back(*i);
-            i++;
-        }
-        else {
-            tempVector.push_back(*j);
-            j++;
-        }
-    }
-    // copy remaining
-    while (i <= end1) {
-        tempVector.push_back(*i);
-        i++;
-    }
-    while (j != end2) {
-        tempVector.push_back(*j);
-        j++;
-    }
-    std::copy(tempVector.begin(), tempVector.end(), start);
-}
 
 void mergeSort(int intArray[], const size_t &length)
 {
@@ -69,32 +38,3 @@ void merge(int intArray[], const std::size_t &end1, const std::size_t &end2)
        intArray[x] = tempArray[x];
 }
 
-/*
-void mergeSort(std::vector<int> &v)
-{
-    //mergeSort(v.begin(),v.end());
-    //std::cout << "mergeSort()\n";
-    typename std::vector<int>::iterator i = v.begin();
-    while (i != v.end() && *i < *(i+1)) {
-        ++i;
-    }
-    if (i == v.end())
-        return;
-    mergeSort(i+1, v.end());
-    merge(v, i);
-}
-*/
-
-/*
-void mergeSort(typename std::vector<int>::iterator start, typename std::vector<int>::iterator finish)
-{
-    typename std::vector<int>::iterator i = start;
-    while (i != finish && *i < *(i+1)) {
-        ++i;
-    }
-    if (i == finish)
-        return;
-    mergeSort(i+1, finish);
-    merge(start, i, finish);
-}
-*/
--- src/mergeSort.h
+++ src/mergeSort.h
@@ -14,11 +14,6 @@
 void merge(int intArray[], const std::size_t &end1, const std::size_t &end2);
 void mergeSort(int intArray[], const size_t &length);
 
-/*
-void merge(std::vector<int>::iterator start, std::vector<int>::iterator end1,
-           std::vector<int>::iterator end2);
-           */
-
 template <class Iter>
 void merge(Iter begin, Iter end1, Iter end2)
 {
@@ -81,114 +76,6 @@ void merge(Container &v, typename Container::iterator begin, typename Container:
     std::copy(tempContainer.begin(), tempContainer.begin()+distance, begin);
 }
 
-/*
-template <typename Container>
-void merge(Container &v, typename Container::const_iterator end1)
-{
-    typename Container::const_iterator end2 = v.end();
-    typename Container::iterator x;
-    typename Container::const_iterator i, j;
-    Container tempContainer(v);
-    for (x = tempContainer.begin(), i = v.begin(), j = end1+1; i <= end1 && j != end2 ; ) {
-        //std::cout << "*i:" << *i << " *j:" << *j << " end1:" << *end1 << " end2:" << *end2 << std::endl;
-        if ((j==end2 || i <= end1) && (*i < *j)) {
-            *x = *i;
-            i++;
-        }
-        else {
-            *x = *j;
-            j++;
-        }
-        x++;
-    }
-    // copy remaining
-    while (i <= end1) {
-        *x = *i;
-        x++;
-        i++;
-    }
-    while (j != end2) {
-        *x = *j;
-        x++;
-        j++;
-    }
-    std::copy(tempContainer.begin(), tempContainer.end(), v.begin());
-}
-*/
-
-/*
-template <typename T, std::size_t size>
-void merge(std::array<T,size> &v, typename std::array<T,size>::const_iterator end1){
-    typename std::array<T,size>::const_iterator end2 = v.end();
-    typename std::array<T,size>::iterator x;
-    typename std::array<T,size>::const_iterator i, j;
-    std::array<T,size> tempContainer;
-    for (x = tempContainer.begin(), i = v.begin(), j = end1+1; i <= end1 && j != end2 ; ) {
-        //std::cout << "*i:" << *i << " *j:" << *j << " end1:" << *end1 << " end2:" << *end2 << std::endl;
-        if ((j==end2 || i <= end1) && (*i < *j)) {
-            *x = *i;
-            i++;
-        }
-        else {
-            *x = *j;
-            j++;
-        }
-        x++;
-    }
-    // copy remaining
-    while (i <= end1) {
-        *x = *i;
-        x++;
-        i++;
-    }
-    while (j != end2) {
-        *x = *j;
-        x++;
-        j++;
-    }
-    std::copy(tempContainer.begin(), tempContainer.end(), v.begin());
-}
-*/
-
-/*
-template <typename Container>
-void merge(Container &v, typename Container::const_iterator end1)
-{
-    typename Container::const_iterator end2 = v.end();
-    typename Container::iterator x;
-    typename Container::const_iterator i, j;
-    Container tempContainer;
-    for (x = tempContainer.begin(), i = v.begin(), j = end1+1; i <= end1 && j != end2 ; ) {
-        //std::cout << "*i:" << *i << " *j:" << *j << " end1:" << *end1 << " end2:" << *end2 << std::endl;
-        if ((j==end2 || i <= end1) && (*i < *j)) {
-            tempContainer.push_back(*i);
-            i++;
-        }
-        else {
-            tempContainer.push_back(*j);
-            j++;
-        }
-    }
-    // copy remaining
-    while (i <= end1) {
-        tempContainer.push_back(*i);
-        i++;
-    }
-    while (j != end2) {
-        tempContainer.push_back(*j);
-        j++;
-    }
-    std::copy(tempContainer.begin(), tempContainer.end(), v.begin());
-}
-*/
-
-/*
-template <class InputIterator>
-void merge(InputIterator begin, InputIterator end1, InputIterator end2) {
-    return;
-}
-*/
-
 template <class InputIterator>
 void mergeSort(InputIterator begin, InputIterator end)
 {
@@ -219,20 +106,5 @@ void mergeSort(Container &c, InputIterator begin, InputIterator end)
     mergeSort(c, i+1, end);
     merge(c, begin, i);
 }
-
-/*
-template <typename Container>
-void mergeSort(Container &c)
-{
-    typename Container::iterator i = c.begin();
-    while (i != c.end() && *i < *(i+1)) {
-        ++i;
-    }
-    if (i == c.end())
-        return;
-    mergeSort(i+1, c.end());
-    merge(c, i);
-}
-*/
 
 #endif // MERGESORT_H
